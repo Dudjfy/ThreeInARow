5e8eeb01f75b1828f85ee05c5fbbf1b87fa8da9a
Dmitry Chirin <chirin.dima@gmail.com>
2021-11-21 19:46:30 +0100

    added comments etc

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 from functools import partial
 from tkinter import *
 
+# Tkinter initialization
 root = Tk()
 root.geometry("600x400")
 root.title("3 In A Row")
 
+
 # Functions and Classes
+
+# Score class to easier mange the scoring system
+class Score:
+    x = 0
+    o = 0
+
+    def __str__(self):
+        return f"X: {self.x}     O: {self.o}"
+
+
+# Game class with all objects to keep track of
 class Game:
     win_combos = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
 
@@ -21,6 +34,7 @@ class Game:
         self.buttons = []
         self.pressed_buttons = []
 
+    # Changes the score for the winner
     def change_score(self, winner):
         if winner == "X":
             self.score_data.x += 1
@@ -29,12 +43,14 @@ class Game:
 
         self.score_label.config(text=self.score_data)
 
+    # Resets score
     def reset_score(self):
         self.score_data.x = 0
         self.score_data.o = 0
         self.score_label.config(text=self.score_data)
 
 
+# Resets the score, can also award points for the winner if specified
 def reset(game, winner):
     game.change_score(winner)
 
@@ -44,16 +60,13 @@ def reset(game, winner):
         button["text"] = ""
 
 
-def reset_everything(game):
-    reset(game, "")
-    game.reset_score()
-
-
+# Checks if all elements in a list are equal, used to determine if there is a winning row
 def all_equal(lst):
     return lst[0] in ["X", "O"] and len(set(lst)) == 1, lst[0]
 
 
-def test_for_win(game):
+# Checks for win combinations
+def check_for_win(game):
     for combo in game.win_combos:
         full_row, winner = all_equal([game.buttons[i]["text"] for i in combo])
         if full_row:
@@ -61,21 +74,24 @@ def test_for_win(game):
     return False, ""
 
 
+# Changes the button text
 def change_txt(game, i):
     if i not in game.pressed_buttons:
         game.buttons[i]["text"] = "X" if game.turn % 2 == 0 else "O"
         game.pressed_buttons.append(i)
         game.turn += 1
 
-        full_row, winner = test_for_win(game)
+        full_row, winner = check_for_win(game)
         if full_row:
             reset(game, winner)
 
 
+# For testing the numbering of buttons
 def try_i(game, i):
     game.buttons[i]["text"] = i
 
 
+# Creates the playing field with buttons to put the Xs and Os
 def create_buttons(game):
     for y in range(3):
         for x in range(3):
@@ -84,41 +100,43 @@ def create_buttons(game):
                        width=game.b_size_x,
                        height=game.b_size_y,
                        command=partial(change_txt, game, x + y * 3))
+                       # command=partial(try_i, game, x + y * 3))  # For testing if the buttons are in correct positions
             b.grid(column=x + 1, row=y + 1)
             game.buttons.append(b)
 
 
-# Label
-class Score:
-    x = 0
-    o = 0
-
-    def __str__(self):
-        return f"X: {self.x}     O: {self.o}"
-
-
-sc = Label(text="X: 0     O: 0", padx=50, pady=20, font=("", 14))
+# Labels
+sc = Label(text="X: 0     O: 0", padx=35, pady=20, font=("", 14))
 sc.grid(column=0, row=0)
 
+# Game object, game buttons
 g = Game(Score(), sc)
 create_buttons(g)
 
-reset_button = Button(root,
-                      text="Reset",
-                      font=("", g.font_size),
-                      width=10,
-                      height=1,
-                      command=partial(reset_everything, g))
-reset_button.grid(column=4, row=0, padx=20, pady=20)
+# Other buttons
+reset_board_button = Button(root,
+                            text="Reset Board",
+                            font=("", g.font_size),
+                            width=12,
+                            height=1,
+                            command=partial(reset, g, ""))
+reset_board_button.grid(column=4, row=0, padx=30, pady=20)
+
+reset_score_button = Button(root,
+                            text="Reset Score",
+                            font=("", g.font_size),
+                            width=12,
+                            height=1,
+                            command=g.reset_score)
+reset_score_button.grid(column=4, row=1, padx=30, pady=20)
 
 exit_button = Button(root,
                      text="Exit",
                      font=("", g.font_size),
-                     width=10,
+                     width=12,
                      height=1,
                      command=root.destroy)
-exit_button.grid(column=4, row=5, padx=20, pady=20)
-
+exit_button.grid(column=4, row=5, padx=30, pady=20)
 
 # Show screen
 root.mainloop()
